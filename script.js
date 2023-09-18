c8a66ac9ebdecf9e40b9e1ace15114b10e9b5bfd
rasmi <rasminoel@gmail.com>
2023-09-18 10:45:59 +0300

    changes in the js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content is added by JavaScript";
+h2.textContent = "...an aspiring web developer";
 document.querySelector("body").appendChild(h2);
