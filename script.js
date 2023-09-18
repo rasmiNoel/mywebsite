35f88b328d6f1d71421441c2c0a90750a5ccd3d0
rasmi <rasminoel@gmail.com>
2023-09-18 14:04:17 +0300

    Add Style

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
 h2.textContent = "...crafting digital solutions with elegant code”";
-document.querySelector("body").appendChild(h2);
+document.querySelector("main").appendChild(h2);
